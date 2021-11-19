32a1bf578f2c2e46ce23680176ea0aca67634631
AkhAbdulaziz <akh.abdulazizs@gmail.com>
2021-11-19 11:44:12 +0500

    Gson, PreferenceHelper, File, utils added

--- MyAndoridKotlinExtensions/src/main/java/uz/gita/myandoridkotlinextensions/ViewExtensions.kt
+++ MyAndoridKotlinExtensions/src/main/java/uz/gita/myandoridkotlinextensions/ViewExtensions.kt
@@ -8,14 +8,6 @@ import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import timber.log.Timber
 
-fun <T : ViewBinding> T.scope(f: T.() -> Unit) {
-    f(this)
-}
-
-fun timber(message: String, tag: String = "TTT") {
-    Timber.tag(tag).d(message)
-}
-
 fun Activity.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, message, duration).show()
 }
@@ -24,8 +16,17 @@ fun Fragment.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this.requireContext(), message, duration).show()
 }
 
+fun View.visible(bool: Boolean) {
+    if (bool) this.visibility = View.VISIBLE
+    else this.visibility = View.GONE
+}
+
 fun View.visible() {
     this.visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    this.visibility = View.INVISIBLE
 }
 
 fun View.gone() {
