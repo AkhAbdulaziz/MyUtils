6392d49128b23048ff3ed8c315b5f8748c3aef76
AkhAbdulaziz <akh.abdulazizs@gmail.com>
2021-11-19 21:45:18 +0500

    Gson, PreferenceHelper, File, utils added

--- MyAndoridKotlinExtensions/src/main/java/uz/gita/myandoridkotlinextensions/MyUtils.kt
+++ MyAndoridKotlinExtensions/src/main/java/uz/gita/myandoridkotlinextensions/MyUtils.kt
@@ -7,31 +7,29 @@ import android.view.inputmethod.InputMethodManager
 import androidx.viewbinding.ViewBinding
 import timber.log.Timber
 
-class MyUtils {
-    fun <T : ViewBinding> T.scope(f: T.() -> Unit) {
-        f(this)
-    }
+fun <T : ViewBinding> T.scope(f: T.() -> Unit) {
+    f(this)
+}
 
-    fun timber(message: String, tag: String = "TTT") {
-        Timber.tag(tag).d(message)
-    }
+fun timber(message: String, tag: String = "TTT") {
+    Timber.tag(tag).d(message)
+}
 
-    //hide keyboard from fragment
-    fun hideKeyboardFrom(context: Context, view: View) {
-        val imm: InputMethodManager =
-            context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.getWindowToken(), 0)
-    }
+//hide keyboard from fragment
+fun hideKeyboardFrom(context: Context, view: View) {
+    val imm: InputMethodManager =
+        context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.getWindowToken(), 0)
+}
 
-    // hide keyboard from activity
-    fun hideKeyboard(activity: Activity) {
-        val imm = activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-        //Find the currently focused view, so we can grab the correct window token from it.
-        var view = activity.currentFocus
-        //If no view currently has focus, create a new one, just so we can grab a window token from it
-        if (view == null) {
-            view = View(activity)
-        }
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
+// hide keyboard from activity
+fun hideKeyboard(activity: Activity) {
+    val imm = activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    //Find the currently focused view, so we can grab the correct window token from it.
+    var view = activity.currentFocus
+    //If no view currently has focus, create a new one, just so we can grab a window token from it
+    if (view == null) {
+        view = View(activity)
     }
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
 }
